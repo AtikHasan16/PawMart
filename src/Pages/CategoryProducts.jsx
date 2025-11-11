f85256e95d4aaadc9bebedf07804dd2424fb8565
AtikHasan16 <akhasannaeem@gmail.com>
2025-11-11 19:02:37 +0600

    added typewriter effect in filtered product page

--- src/Pages/CategoryProducts.jsx
+++ src/Pages/CategoryProducts.jsx
@@ -19,9 +19,9 @@ const CategoryProducts = () => {
   //console.log(data);
 
   return (
-    <div className=" min-h-[calc(100dvh-400px)] sand">
+    <div className=" min-h-[calc(100dvh-400px)] sand my-6">
       <title>Category-Products</title>
-      <h2 className="text-3xl md:text-4xl font-bold text-center mb-12 bg-primary rounded-full w-fit mx-auto py-4 px-6 text-secondary">
+      <h2 className="text-3xl md:text-4xl font-bold text-center mb-8  bg-primary rounded-full w-fit mx-auto py-4 px-6 text-secondary">
         <Typewriter words={["Filtered by Category"]} />
       </h2>
       <Container>
