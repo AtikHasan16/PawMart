353c404b4373936eea4a8b73b6dba6086d092643
AtikHasan16 <akhasannaeem@gmail.com>
2025-11-13 16:41:57 +0600

    added go back button in error page

--- src/Error/ErrorPage.jsx
+++ src/Error/ErrorPage.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { Link } from "react-router";
 
 const ErrorPage = () => {
   return (
-    <div className="h-screen content-center text-center sand text-9xl text-gray-400">
+    <div className="h-screen content-center text-center sand">
       <title>Error</title>
-      <h1 className="font-bold"> 404 </h1>
-      <h2 className="font-semibold text-3xl">Page Not Found</h2>
+      <div className=" text-gray-400">
+        <h1 className="font-bold   text-9xl"> 404 </h1>
+        <h2 className="font-semibold text-3xl">Page Not Found</h2>
+        <Link to={"/"} className="font-bold border-2 btn mt-4">
+          Back to Home
+        </Link>
+      </div>
     </div>
   );
 };
