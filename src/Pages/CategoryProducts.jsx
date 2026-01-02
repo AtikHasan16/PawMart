7ddf63e1655648a861a134286c69041a8d2a86c5
Hasan MD Atik <akhasannaeem@gmail.com>
2026-01-02 08:52:58 +0600

    fix layout problem

--- src/Pages/CategoryProducts.jsx
+++ src/Pages/CategoryProducts.jsx
@@ -30,7 +30,7 @@ const CategoryProducts = () => {
             </h1>
           </div>
         ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 p-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 p-4">
             {data.map((listing) => (
               <ListingCard key={listing._id} listing={listing}></ListingCard>
             ))}
--- src/Pages/PetsSupply.jsx
+++ src/Pages/PetsSupply.jsx
@@ -137,7 +137,7 @@ const PetsSupply = () => {
             ) : (
               <motion.div
                 {...motionProps}
-                className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5 p-4 "
+                className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 p-4"
               >
                 {searchData.map((listing) => (
                   <ListingCard
