2880565440a7aa45206fe947858a7f18b5a16cf0
Hasan MD Atik <akhasannaeem@gmail.com>
2026-01-02 08:24:51 +0600

    fixed home card grid

--- src/Components/Home/RecentProducts.jsx
+++ src/Components/Home/RecentProducts.jsx
@@ -3,11 +3,11 @@ import { useLoaderData } from "react-router";
 import ListingCard from "../Shared/LstingCard";
 
 const RecentProducts = () => {
-  const { data } = useLoaderData();
-  //console.log(data);
+  const recentData = useLoaderData();
+  const data = recentData.data.slice(0, 4);
 
   return (
-    <div className="my-12">
+    <div className="my-8">
       <h2 className="text-3xl md:text-4xl font-bold text-center mb-6 bg-primary rounded-full w-fit mx-auto py-4 px-6 text-secondary">
         Recent Items
       </h2>
@@ -21,7 +21,7 @@ const RecentProducts = () => {
           </div>
         </div>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 p-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 p-4">
           {data.map((listing) => (
             <ListingCard key={listing._id} listing={listing}></ListingCard>
           ))}
