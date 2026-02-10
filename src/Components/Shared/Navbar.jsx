67d9c44ca0d075718c6db7e970fc27da48281577
Hasan MD Atik <akhasannaeem@gmail.com>
2026-02-10 14:35:43 +0600

    feat: Add logout confirmation modal using SweetAlert2 in Navbar component

--- src/Components/Shared/Navbar.jsx
+++ src/Components/Shared/Navbar.jsx
@@ -14,6 +14,8 @@ import {
   FaEnvelope,
   FaQuestionCircle,
 } from "react-icons/fa";
+import Swiper from "swiper";
+import Swal from "sweetalert2";
 
 const Navbar = () => {
   const { currentUser, logOutUser, loading } = use(AuthContext);
@@ -90,13 +92,31 @@ const Navbar = () => {
       </li>
     ));
   const handleLogout = () => {
-    logOutUser()
-      .then(() => {
-        toast.success("Successfully signout");
-      })
-      .catch((err) => {
-        toast.error(err.code);
-      });
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      logOutUser()
+        .then(() => {
+          toast.success("Successfully signout");
+        })
+        .catch((err) => {
+          toast.error(err.code);
+        });
+
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success",
+        });
+      }
+    });
   };
 
   return (
